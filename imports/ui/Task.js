c54d04f526aa970b15b3c2a15d1394dd9f390022
Chris <chris.hoffmann84@gmail.com>
2019-07-23 13:06:31 +0200

    added delete tasks story

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { Tasks } from '../api/tasks.js';
 
 export default class Task extends Component {
+    deleteItem() {
+        Tasks.remove(this.props.task._id);
+    }
+
     render() {
-        return (<li>{ this.props.task.text }</li>);
+        return (
+            <li>
+                <button className="delete" onClick={ this.deleteItem.bind(this) }>&times;</button>
+                <span>{ this.props.task.text }</span>
+            </li>
+        );
     }
 }
