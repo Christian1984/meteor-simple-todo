c173afa0de0f152a07e3d22346f456c0c6f3b35c
Chris <chris.hoffmann84@gmail.com>
2019-07-23 10:25:22 +0200

    added basic task mockup

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+    getTasks()
+    {
+        return [
+            { _id: 1, text: 'This is Task 1'},
+            { _id: 2, text: 'This is Task 2'},
+            { _id: 3, text: 'This is Task 3'}
+        ];
+    }
+
+    renderTasks()
+    {
+        return this.getTasks().map(task => (<li key={ task._id }>{ task.text }</li>));
+    }
+
     render() {
         return (
             <div className="container">
-                <h1>Todo List</h1>
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+                <div>
+                    <ul>{ this.renderTasks() }</ul>
+                </div>
             </div>
         );
     }
