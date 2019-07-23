65e026b5a09162653f0702981148de2643df1bef
Chris <chris.hoffmann84@gmail.com>
2019-07-23 13:34:48 +0200

    added check tasks story

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -6,10 +6,22 @@ export default class Task extends Component {
         Tasks.remove(this.props.task._id);
     }
 
+    toggleCheckbox(e) {
+        Tasks.update(this.props.task._id, { 
+            $set: { checked: !this.props.task.checked }
+        });
+    }
+
     render() {
         return (
             <li>
                 <button className="delete" onClick={ this.deleteItem.bind(this) }>&times;</button>
+                <input 
+                    type="checkbox"
+                    readOnly
+                    checked={ !!this.props.task.checked }
+                    onClick={ this.toggleCheckbox.bind(this) }
+                />
                 <span>{ this.props.task.text }</span>
             </li>
         );
